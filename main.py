a2ad9023508d31713724915a5a4af1f768441801
b95rkumar <70858650+b95rkumar@users.noreply.github.com>
2023-05-31 08:09:21 +0000

    dynamic JOBS

--- main.py
+++ main.py
@@ -2,9 +2,35 @@ from flask import Flask, render_template
 
 app= Flask (__name__)
 
+JOBS=[
+  {
+  'id':1,
+  'title':'Data Scietist',
+  'location':'New Delhi, India',
+  'salary':'Rs 10,00,000'
+  },
+  {
+  'id':2,
+  'title':'Data Analyst',
+  'location':'Bengurule, India',
+  'salary':'Rs 12,00,000'
+  },
+  {
+  'id':3,
+  'title':'Data Base expert',
+  'location':'USA',
+  'salary':'$ 110,000'
+  },
+  {
+  'id':4,
+  'title':'Data Scietist Manager',
+  'location':'New Delhi, India'
+  }
+]
+
 @app.route ("/")
 def hello_world ():
-  return render_template ( "home.html")
+  return render_template ( "home.html", jobs=JOBS, company_name="Jovian")
 
 if __name__ == "__main__" :
   app.run(host="0.0.0.0", debug=True)
